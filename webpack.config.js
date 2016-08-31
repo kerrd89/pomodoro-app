f9e72bb172ad7a9c123e3c68df63915774bc8d82
Blake Worsley <blakeworsley@gmail.com>
2016-08-31 13:06:43 -0600

    fixed images

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,15 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015' },
-      { test: /\.svg/, loader: "svg-url-loader" },
-      { test: /\.png$/, loader: "url-loader",
-      query: { mimetype: "image/png" }},
+      // { test: /\.svg/, loader: "svg-url-loader" },
+      // { test: /\.png$/, loader: "url-loader", query: { mimetype: "image/png" }},
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      },
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loader: "style!css!resolve-url!sass?sourceMap" }
     ]
