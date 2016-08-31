79fc1b0769719ffcbc2f4124ddcb96c7a23cc390
Blake Worsley <blakeworsley@gmail.com>
2016-08-31 13:31:43 -0600

    changed slider size

--- webpack.config.js
+++ webpack.config.js
@@ -9,20 +9,11 @@ module.exports = {
     path: __dirname,
     filename: "[name].bundle.js"
   },
-  // module: {
-  // loaders: [
-  //   // { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
-  //   { test: /\.css$/, loader: "style!css!" },
-  //   { test: /\.scss$/, loader: "style!css!sass" }
-  // ]},
 
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015' },
-      // { test: /\.svg/, loader: "svg-url-loader" },
-      // { test: /\.png$/, loader: "url-loader", query: { mimetype: "image/png" }},
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
+      { test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
         'file?hash=sha512&digest=hex&name=[hash].[ext]',
         'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
